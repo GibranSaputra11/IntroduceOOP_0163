9ca835f90ff9ac13c102c532ead6c19f29ea578b
GibranSaputra11 <gibranbantul@gmail.com>
2025-04-28 14:50:54 +0700

    add class hewan

---
+++ Class.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Hewan
+{ // memiliki 3 member
+private:
+    char jenis_kelamin; // memggunkan char karena hanya menggunakn 1 kareakter anatar P atau L
+public:
+    string tipe;
+    string umur; // memnggunakn string karena tidak akan dikoputasi
+    void inputData(){ // membuat member berupa prilaku atau preosedur matau method
+        cout << "Masukan Jenis Kelamin : ";
+        cin >> jenis_kelamin;
+        cout << "Masukan Tipe Hewan (Darat, Udara, Air) : ";
+        cin >> tipe;
+        cout << "Masukan Umur Hewan : ";
+        cin >> umur;
+    }
+
+}; // harus menggunakn titik koma ketika menggunkana clas
+
+int main()
+{
+    
+
+}
